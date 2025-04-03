4b101c1d49848e24b12c1252a52e6a8d96d0a600
Max1357531 <max.kilburn99@gmail.com>
2025-04-03 16:19:26 +0100

    add topic query to articles page

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,16 +1,19 @@
 import { getArticles } from "../utils/api"
 import { useData } from "../hooks/useData"
 import { ArticleCard } from "./ArticleCard"
+import { useSearchParams } from "react-router-dom"
 
 
 export const Articles = () =>{
-    const {data: articles, loading,error} = useData(getArticles)
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    const {data: articles, loading,error} = useData(getArticles,searchParams.get("topic"))
 
     if (loading){
         return (<div></div>)
     }
     else if (error){
-        return <p>Error</p>
+        return <p>404 Not Found</p>
     }
     else{
 
